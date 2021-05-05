84d6f7032ede781409777044b98d76021806ddc3
Nat <wmainvielle@gmail.com>
2021-05-05 16:14:27 +0200

    2.0.14 - Little patch

--- src/Utils.js
+++ src/Utils.js
@@ -10,7 +10,7 @@ module.exports = class Util {
 		if ([target, source].some((e) => typeof e != 'object' || Array.isArray(e))) throw new TypeError('ECHIDNA_INVALID_OPTION', 'target|source', 'object')
 		for (const [key, value] of Object.entries(source)) {
 			if (Array.isArray(value) && !Array.isArray(target[key])) target[key] = []
-			else if (typeof value == 'object') target[key] = checkTypings(target[key] || {}, value)
+			else if (typeof value == 'object') target[key] = Util.checkTypings(target[key] || {}, value)
 			else if (typeof value != typeof target[key]) target[key] = value
 		}
 		return target
