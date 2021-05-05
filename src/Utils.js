d212fb620dc36084e570a6f307e3617014743792
Nat <wmainvielle@gmail.com>
2021-05-05 16:31:34 +0200

    2.0.16 - Util patch

--- src/Utils.js
+++ src/Utils.js
@@ -7,7 +7,7 @@ module.exports = class Util {
 	}
 
 	static checkTypings (target, source) {
-		if ([target, source].some((e) => typeof e != 'object' || Array.isArray(e))) throw new TypeError('ECHIDNA_INVALID_OPTION', 'target|source', 'object')
+		if ([target, source].some((e) => typeof e != 'object')) throw new TypeError('ECHIDNA_INVALID_OPTION', 'target|source', 'object')
 		for (const [key, value] of Object.entries(source)) {
 			if (Array.isArray(value) && !Array.isArray(target[key])) target[key] = []
 			else if (typeof value == 'object') target[key] = Util.checkTypings(target[key] || {}, value)
